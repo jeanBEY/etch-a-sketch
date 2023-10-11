1ba5f0d063a82da789ebefacbc53406a55d5abec
Jeanvally G.Beato <jeanvallybeato@gmail.com>
2023-10-11 13:17:52 -0700

    Put each task into a function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,3 @@
-
-//********** CREATE INITIAL GRID ON LOAD (5 x 5 ) **********
-
 //CREATES GRID ITSELF
 //RESOURCE:  https://www.reddit.com/r/learnjavascript/comments/jf19y6/creating_a_grid_using_divs/
 function createRowDiv(x, y){
@@ -21,41 +18,47 @@ function createRow(i, y, rowDiv){
     }
 }
 
-
-
-createRowDiv(5,5);
-
-
-
 //ADDS FLEXBOX
-//RESOURCE: https://css-tricks.com/dont-overthink-flexbox-grids/
-const rowList = document.querySelectorAll(".rowDiv");
-for(let i = 0; i < rowList.length; i++){
-    rowList[i].style.display = "flex";
-}
+function addFlexbox(){
 
-//RESOURCE: https://stackoverflow.com/questions/29307971/css-grid-of-squares-with-flexbox
-const cellList = document.querySelectorAll(".cell");
-for(let i = 0; i < cellList.length; i++){
-    cellList[i].style.flexGrow = 1;
-    cellList[i].style.flexShrink = 0;
-    cellList[i].style.flexBasis = 0;
-    cellList[i].style.aspectRatio = 1/1;
-}
+    //RESOURCE: https://css-tricks.com/dont-overthink-flexbox-grids/
+    const rowList = document.querySelectorAll(".rowDiv");
+    for(let i = 0; i < rowList.length; i++){
+        rowList[i].style.display = "flex";
+    }
 
+    //RESOURCE: https://stackoverflow.com/questions/29307971/css-grid-of-squares-with-flexbox
+    const cellList = document.querySelectorAll(".cell");
+    for(let i = 0; i < cellList.length; i++){
+        cellList[i].style.flexGrow = 1;
+        cellList[i].style.flexShrink = 0;
+        cellList[i].style.flexBasis = 0;
+        cellList[i].style.aspectRatio = 1/1;
+    }
+
+}
 
 //HOVERING EFFECT
-//RESOURCE: https://stackoverflow.com/questions/52563010/mouseenter-mouseleave-in-epmty-div-change-a-color-and-remove-it
-const boxes = document.querySelectorAll(".cell");
-for(let i = 0; i < boxes.length; i++){
-    boxes[i].addEventListener('mouseenter', colorenter);
-    boxes[i].addEventListener('mouseleave', colorleave);
+function addHovering(){
+
+    //RESOURCE: https://stackoverflow.com/questions/52563010/mouseenter-mouseleave-in-epmty-div-change-a-color-and-remove-it
+    const boxes = document.querySelectorAll(".cell");
+    for(let i = 0; i < boxes.length; i++){
+        boxes[i].addEventListener('mouseenter', colorenter);
+        boxes[i].addEventListener('mouseleave', colorleave);
+    }
+
+    function colorenter(e){
+        e.target.style.backgroundColor = "white";
+    }
+
+    function colorleave(e){
+        e.target.style.backgroundColor = "lightsteelblue";
+    }
+
 }
 
-function colorenter(e){
-    e.target.style.backgroundColor = "white";
-}
-
-function colorleave(e){
-    e.target.style.backgroundColor = "lightsteelblue";
-}
+/*CREATE INITIAL GRID ON LOAD*/
+createRowDiv(5,5);
+addFlexbox();
+addHovering();
